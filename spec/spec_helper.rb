40052ae6d25836b090aa36ed468f178ea27ff1af
Joost Baaij <joost@spacebabies.nl>
2011-08-12 10:47:53 +0200

    move database support to spec/support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,32 +1,12 @@
-$LOAD_PATH << "." unless $LOAD_PATH.include?(".")
+require 'rubygems'
+require 'bundler/setup'
 
-require 'yaml'
-require File.dirname(__FILE__) + '/../rails/init.rb'
+require 'nilly_vanilly'
 
-ENV['DB'] ||= 'sqlite3'
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-database_yml = File.expand_path('../database.yml', __FILE__)
-if File.exists?(database_yml)
-  active_record_configuration = YAML.load_file(database_yml)[ENV['DB']]
-  
-  ActiveRecord::Base.establish_connection(active_record_configuration)
-  ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
-  
-  ActiveRecord::Base.silence do
-    ActiveRecord::Migration.verbose = false
-    
-    load(File.dirname(__FILE__) + '/schema.rb')
-    load(File.dirname(__FILE__) + '/models.rb')
-  end  
-  
-else
-  raise "Please create #{database_yml} first to configure your database. Take a look at: #{database_yml}.sample"
+RSpec.configure do |config|
+  # some (optional) config here
 end
-
-def clean_database!
-  [Post, Comment].each do |model|
-    ActiveRecord::Base.connection.execute "DELETE FROM #{model.table_name}"
-  end
-end
-
-clean_database!
